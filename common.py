d6884796c9599b752497483581f28929f9b3d907
tobran <1169622523@qq.com>
2019-04-13 19:58:18 +0800

    add common.py

---
+++ common.py
@@ -0,0 +1,11 @@
+﻿import torch
+
+class des:
+	def __init__(self,x):
+	
+		self.x = x
+	
+	def describe(self):
+		print("Type: {}".format(self.x.type()))
+		print("Shape/size: {}".format(self.x.shape))
+		print("Values: \n{}".format(self.x))
